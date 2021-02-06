e2e7713804f65331fbb65e77f59335e8a73b5220
Jean Amadeu <jean.amadeu2@gmail.com>
2021-02-06 16:29:07 -0300

    Criado classe Conta

---
+++ target/classes/br/com/alura/jpa/modelo/Conta.java
@@ -0,0 +1,17 @@
+package br.com.alura.jpa.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Conta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private Integer agencia;
+	private Integer numero;
+	private String titular;
+}
